4a85c70d44d4f689dde3aeac1bae889057b5065d
hyeminem-Jo <konnimey@naver.com>
2022-04-06 19:52:16 +0900

    🩹 Change files name

--- js/_section-header.js
+++
@@ -1,8 +0,0 @@
-const $headerIconBtn = document.querySelector('.product-shipment .product-section-header.sm-only .icon-button');
-
-function openProductShipment() {
-  const $productShipment = this.parentNode.parentNode
-  $productShipment.classList.add('is-open');
-}
-
-$headerIconBtn.addEventListener('click', openProductShipment);
